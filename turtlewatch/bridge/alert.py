3e5533b0c861947bd5a3744a751687a254284d23
j4n-r <jan.rueggeberg@pm.me>
2025-12-16 20:44:44 +0100

    integrate alert system

--- turtlewatch/bridge/alert.py
+++ turtlewatch/bridge/alert.py
@@ -1,29 +1,41 @@
-import logging
 import os
 import ssl
-import certifi 
-from dotenv import load_dotenv
+import certifi
+import logging
+from datetime import datetime, timedelta
 from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 
 logger = logging.getLogger("BridgeLogger")
 
-def send_slack_message(channel_id: str, message_text: str):
-    """ Calling it send_slack_message("C09PRS9P08K", "Test with fixed SSL")"""
-    ssl_context = ssl.create_default_context(cafile=certifi.where())
-    client = WebClient(
-        token=os.environ.get("SLACK_TOKEN"),
-        ssl=ssl_context  
-    )
+class AlertSystem:
+    last_alert_time = None
+    COOLDOWN_SECONDS = 60  
 
-    try:
-        result = client.chat_postMessage(
-            channel=channel_id, 
-            text=message_text
+    @classmethod
+    def send_slack_message(cls, channel_id: str, message_text: str):
+        if cls.last_alert_time:
+            time_since_last = datetime.now() - cls.last_alert_time
+            if time_since_last < timedelta(seconds=cls.COOLDOWN_SECONDS):
+                wait_time = cls.COOLDOWN_SECONDS - time_since_last.total_seconds()
+                return 
+
+        # SSL Context setup
+        ssl_context = ssl.create_default_context(cafile=certifi.where())
+        client = WebClient(
+            token=os.environ.get("SLACK_TOKEN"),
+            ssl=ssl_context  
         )
-        logger.info(f"Message sent successfully: {result['ts']}")
 
-    except SlackApiError as e:
-        logger.error(f"Error sending message: {e.response['error']}")
+        try:
+            result = client.chat_postMessage(
+                channel=channel_id, 
+                text=message_text
+            )
+            
+            cls.last_alert_time = datetime.now()
+            
+            logger.info(f"Message sent successfully: {result['ts']}")
 
-
+        except SlackApiError as e:
+            logger.error(f"Error sending message: {e.response['error']}")
